b80ac297e64f18d2152b938c150bb7930480fe71
maparent@acm.org <maparent@acm.org>
2012-08-06 16:38:40 -0400

    base64; exifxml; list of files from a list of dirs

--- utils.py
+++ utils.py
@@ -1,18 +1,43 @@
 from os import walk
-from os.path import join
+from os.path import join, exists
+from base64 import b64decode
+from subprocess import check_call
 
-example_file_dirs = ['/Users/maparent/OpenSource/Image-ExifTool-8.61/t/images/',  'tests']
-example_files = []
-endings = ['.exif.xml', '.results.txt']
+from lxml import etree
 
-for example_file_dir in example_file_dirs:
-	for root, dirs, files in walk(example_file_dir, True):
-		dirs[:] = [] # do not recurse
-		for a_file in files:
-			skip = False
-			for ending in endings:
-				if a_file.endswith(ending):
-					skip = True
-					break
-			if not skip:
-				example_files.append(join(root,a_file))
+
+def getExampleFiles(example_file_dirs):
+    example_files = []
+    endings = ['.exif.xml', '.results.txt']
+    for example_file_dir in example_file_dirs:
+        for root, dirs, files in walk(example_file_dir, True):
+            dirs[:] = []  # do not recurse
+            for a_file in files:
+                skip = False
+                for ending in endings:
+                    if a_file.endswith(ending):
+                        skip = True
+                        break
+                if not skip:
+                    example_files.append(join(root, a_file))
+    return example_files
+
+
+def base64decode(s):
+    try:
+        b = b64decode(s.strip())
+    except:
+        return s
+    for enc in ('utf-8', 'iso-8859-1'):
+        try:
+            return b.decode(enc)
+        except:
+            pass
+    return b
+
+
+def getExifXml(fname):
+    fname2 = fname + '.exif.xml'
+    if not exists(fname2):
+        check_call(['exiftool', '-X', '-w', '%d%f.%e.exif.xml', fname])
+    return etree.parse(fname2)
