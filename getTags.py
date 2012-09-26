2eec01059f61d2a516a3620db302b9eebd39b29c
Marc-Antoine Parent <maparent@acm.org>
2012-09-25 21:34:10 -0400

    linting; no tags base file

--- getTags.py
+++ getTags.py
@@ -3,19 +3,24 @@
 import sys
 from multiprocessing import Pool, cpu_count
 from utils import getExampleFiles, getExifXml
-from tags import tags as old_tags
+try:
+    from tags import tags as old_tags
+except ImportError:
+    old_tags = []
 
 from namespaces import ns
 
 setns = set(ns.values())
 
+
 def nameElements(node):
     tag = node.tag
     if '}' in tag:
         if node.prefix not in ns:
-            print "MISSING:"+node.prefix+' '+tag.split('}',1)[0]
-        return (node.prefix, tag.split('}',1)[0][1:], tag.split('}',1)[1])
+            print "MISSING:" + node.prefix + ' ' + tag.split('}', 1)[0]
+        return (node.prefix, tag.split('}', 1)[0][1:], tag.split('}', 1)[1])
     return (None, None, tag)
+
 
 def getKeys(fname):
     content = getExifXml(fname)
@@ -23,18 +28,20 @@ def getKeys(fname):
     names = set([nameElements(node) for node in nodes])
     return names
 
+
 def writeNames(old_tags, new_tags):
-    with open('tags.py','w') as f:
+    with open('tags.py', 'w') as f:
         f.write("tags=[\n")
         for (pre, uri, local) in old_tags:
-            f.write("('%s','%s','%s'),\n"%(pre, uri, local))
+            f.write("('%s','%s','%s'),\n" % (pre, uri, local))
         if new_tags:
             print "--- New tags!"
             f.write("# new tags\n")
             for (pre, uri, local) in new_tags:
-                print "('%s','%s','%s'),\n"%(pre, uri, local)
-                f.write("('%s','%s','%s'),\n"%(pre, uri, local))
+                print "('%s','%s','%s'),\n" % (pre, uri, local)
+                f.write("('%s','%s','%s'),\n" % (pre, uri, local))
         f.write(']\n')
+
 
 if __name__ == '__main__':
     pool = Pool(cpu_count())
